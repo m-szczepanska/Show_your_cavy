dd81651e65e12ae7e1452e79f1b36e1424be20df
Maria Szczepańska <szczepanskamaria93@gmail.com>
2019-12-25 16:45:32 +0100

    Add S3 settings.

--- production_example.py
+++ production_example.py
@@ -1,6 +1,9 @@
 from settings import *
 
 
+ALLOWED_HOSTS.append("http://your_public_dns_ipv4,your_public_dns_ipv4")
+ALLOWED_HOSTS.append("*")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -11,3 +14,11 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', 'ACTUAL_VALUE_PROBABLY_5432'),
     }
 }
+
+# Always use S3
+AWS_DEFAULT_ACL = 'public-read'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
+AWS_ACCESS_KEY_ID = 'your_access_key_id'
+AWS_SECRET_ACCESS_KEY = 'your_secret_access_key'
+AWS_STORAGE_BUCKET_NAME = 'squeakoo-static'
